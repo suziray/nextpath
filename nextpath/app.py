e5001e7a7434e7911be085a9816b89747e940a48
Chaoping (Charles) Deng <cd446@cornell.edu>
2018-05-04 12:39:19 -0400

    delete flash

--- nextpath/app.py
+++ nextpath/app.py
@@ -183,7 +183,6 @@ def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
             return redirect(request.url)
         file = request.files['file']
         # if user does not select file, browser also
