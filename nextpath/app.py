2d0688e2bcf698c322de6710e148d22d433e0289
Chaoping (Charles) Deng <cd446@cornell.edu>
2018-05-04 12:48:35 -0400

    redirect to index

--- nextpath/app.py
+++ nextpath/app.py
@@ -192,7 +192,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('authorized'))
+            return redirect(url_for('index'))
     return render_template("upload.html")
 
 if __name__ == '__main__':
