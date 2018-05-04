a49b5d5a488a5a5489380fb9c6bebade3c6a5f90
Chaoping (Charles) Deng <cd446@cornell.edu>
2018-05-04 16:24:25 -0400

    fix

--- nextpath/app.py
+++ nextpath/app.py
@@ -273,11 +273,10 @@ def dashboard():
     sql = "SELECT company.name FROM company where company.id= '" + company_id +"'"
     cur.execute(sql)
     name = cur.fetchall()
-    cur = g.db_conn.cursor()
     sql = "SELECT project.title, project.skill, project.description, companyprojectrel.start_time, companyprojectrel.expire_time, company.url, companyprojectrel.time_limit FROM project,companyprojectrel,company where project.id=companyprojectrel.project_id AND companyprojectrel.company_id= '" + company_id +"'"
     cur.execute(sql)
     projects = cur.fetchall()
-    return render_template('dashboard.html', challenges=projects, company_name=name)
+    return render_template('dashboard.html', challenges=projects, company_name=name[0][0])
 
 
 
