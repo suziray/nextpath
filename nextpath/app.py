949358adaf44dc4499b16bab28f066d14acbd0ec
Chaoping (Charles) Deng <cd446@cornell.edu>
2018-05-04 11:36:06 -0400

    commit upload page

--- nextpath/app.py
+++ nextpath/app.py
@@ -7,9 +7,15 @@ from flask_debugtoolbar import DebugToolbarExtension
 import logging
 import json
 
+from werkzeug.utils import secure_filename
+ 
+UPLOAD_FOLDER = '/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 toolbar = DebugToolbarExtension(app)
 oauth = OAuth(app)
 
@@ -155,6 +161,32 @@ def challenges():
         logging.warning(valid_projects)
         return render_template('challenges.html', challenges=valid_projects)
     return redirect(url_for('login'))
+
+
+####upload files######
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+ 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
 
 
 if __name__ == '__main__':
